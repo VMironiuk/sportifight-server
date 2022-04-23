3468bc611f570d071aaeb6c0017019323f3b8811
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-23 17:31:43 +0300

    Inject the handler to the server

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"sportifight"
+	"sportifight/handler"
 )
 
 func main() {
+	handlers := handler.New()
 	srv := new(sportifight.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("an error occured during server start: %s\n", err.Error())
 	}
 }
