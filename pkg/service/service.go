f7737929e4c23842144631742f9b17ac4e881d92
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-25 16:29:34 +0300

    Add Auth interface. Inject AuthService to Service

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,7 +1,17 @@
 package service
 
-type Service struct{}
+import "github.com/VMironiuk/sportifight-server"
+
+type Auth interface {
+	CreateUser(user sportifight.User) (int, error)
+}
+
+type Service struct {
+	Auth
+}
 
 func NewService() *Service {
-	return &Service{}
+	return &Service{
+		Auth: NewAuthService(),
+	}
 }
