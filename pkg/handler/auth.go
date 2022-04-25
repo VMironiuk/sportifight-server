f5d0b12152c06a01b28f28fd4e4a330b0c98613f
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-25 16:42:27 +0300

    Changes to the previous commit

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -3,13 +3,29 @@ package handler
 import (
 	"net/http"
 
+	"github.com/VMironiuk/sportifight-server"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) signUp(ctx *gin.Context) {
-	// TODO: add implementation
-	ctx.JSON(http.StatusOK, map[string]string{
-		"ok": "sign-up test response",
+	var input sportifight.User
+	if err := ctx.BindJSON(&input); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	id, err := h.service.Auth.CreateUser(input)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, map[string]int{
+		"id": id,
 	})
 }
 
