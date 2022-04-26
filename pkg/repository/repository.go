d9f9ac8608b960bdafdbc9c5f106f643c5f93158
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-26 19:29:32 +0300

    Add Auth interface into repository package

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,6 +1,14 @@
 package repository
 
-type Repository struct{}
+import "github.com/VMironiuk/sportifight-server"
+
+type Auth interface {
+	CreateUser(user sportifight.User) (int, error)
+}
+
+type Repository struct {
+	Auth
+}
 
 func New() *Repository {
 	return &Repository{}
